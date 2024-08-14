79ef81b0341190524d717bf6b78e029551b9d8b8
Alex Kondratiuk <alexozavr007@gmail.com>
2024-08-14 12:49:37 +0300

    Add project files.

---
+++ HomeWork4_13_2/Program.cs
@@ -0,0 +1,26 @@
+﻿static class Program {
+
+    static void Method1() {
+        Thread.CurrentThread.IsBackground = false;
+        Console.WriteLine($"Method1: Start ({Thread.CurrentThread.ManagedThreadId} / {Task.CurrentId})");
+        Thread.Sleep(1500);
+        Console.WriteLine("Method1: End");
+    }
+
+    static void Method2() {
+        Thread.CurrentThread.IsBackground = false;
+        Console.WriteLine($"Method2: Start ({Thread.CurrentThread.ManagedThreadId} / {Task.CurrentId})");
+        Thread.Sleep(1500);
+        Console.WriteLine("Method2: End");
+    }
+
+    static void Main() {
+        Parallel.Invoke(new ParallelOptions { MaxDegreeOfParallelism = 2 },
+            () => Task.Run(() => Method1()),
+            () => Task.Run(() => Method2())
+        );
+
+        Console.WriteLine($"Main thread ({Thread.CurrentThread.ManagedThreadId})");
+    }
+
+}
---
+++ HomeWork4_13_4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+
+int[] array = new int[10000000];
+Random random = new Random();
+for (int i = 0; i < array.Length; i++) {
+    array[i] = random.Next();
+}
+var odds = array.AsParallel().Where( x => x % 2 == 1 );
+
+Console.WriteLine("Перші 10 непарних:");
+
+var counter = 0;
+
+foreach(var odd in odds) {
+
+    Console.WriteLine(odd);
+
+    counter++;
+
+    if (counter ==10)
+        break;
+    
+}
